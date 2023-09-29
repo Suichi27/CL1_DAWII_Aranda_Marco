e682b8887433467b769a2fd8065aa06454aae685
MarcoAranda <umauricio2705@gmail.com>
2023-09-29 04:24:22 -0500

    Inicio Examen

---
+++ src/main/java/com/examen/CL1_DAWII_Aranda_Marco/Cl1DawiiArandaMarcoApplication.java
@@ -0,0 +1,13 @@
+package com.examen.CL1_DAWII_Aranda_Marco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cl1DawiiArandaMarcoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cl1DawiiArandaMarcoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/examen/CL1_DAWII_Aranda_Marco/Cl1DawiiArandaMarcoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.examen.CL1_DAWII_Aranda_Marco;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cl1DawiiArandaMarcoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
